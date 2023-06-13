aa4d346e6b37c92bbd05067900ed4f232e185c90
SamFKA <115175087+SamFKA@users.noreply.github.com>
2023-06-13 14:35:43 -0500

    Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: limegreen">
-          Samagram Two
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: coral">
+          Samagram Three
         </b-navbar-item>
       </template>
     </b-navbar>
